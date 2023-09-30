0ca7b4f4aff1566d29cacbdccf79012d7b70775d
TheMCLionSheep <48334035+TheMCLionSheep@users.noreply.github.com>
2023-09-30 17:52:12 -0400

    Created new room system
    Created a new room system that allows players to create a room with a random room code, and then have other players join the room via the correct room code.
    Players in the same room can chat with each other. Rooms are automatically destroyed when players leave, and if players are already in a room, they can't join another.
    
    1.4 h

--- server/index.js
+++ server/index.js
@@ -5,96 +5,101 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server)
 
+const MAX_PLAYERS = 4;
 
-class ServerRooms {
-    rooms = [];
-
-    addRoom(room) {
-        this.rooms.push(room);
-    }
-
-    findOpenRoom() {
-        for (let room of this.rooms) {
-            if (room.getSize() < 3) {
-                return room
-            }
-        }
-        return null
-    }
-
-    findPlayerRoom(player) {
-        console.log(this.rooms)
-        for (let room of this.rooms) {
-            if (room.players.includes(player)) {
-                return room
-            }
-        }
-        return null
-    }
-}
-
-class Room {
-    constructor(roomId, players) {
-        this.roomId = roomId;
-        this.players = players;
-    }
-
-    // Join a room with a player id if there's space
-    joinRoom(player) {
-        if (this.players.length < 3) {
-            this.players.push(player);
-        }
-        else {
-            console.log("No space in room " + this.roomId);
-        }
-    }
-
-    getSize() {
-        return this.players.length;
-    }
-}
-
-rooms = new ServerRooms()
+// Pairs client ID to their room
+var clientRooms = {}
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected' + socket.id);
+io.on('connection', (client) => {
+    console.log('A user connected ' + client.id);
 
-    socket.on('chat message', (msg) => {
-        room = rooms.findPlayerRoom(socket.id);
-        if (room) {
-            console.log("to room: " + room.roomId)
-            socket.to(room.roomId).emit('chat message', msg);
+    client.on('send-message', sendMessage);
+    client.on('start-room', startRoom);
+    client.on('join-room', joinRoom);
+
+    client.on('disconnect', () => {
+        console.log('A user disconnected ' + client.id);
+        if (client.id in clientRooms) {
+            delete clientRooms[client.id];
+        }
+        console.log(clientRooms)
+    });
+
+    function sendMessage(msg) {
+        // Client is in a room
+        if (client.id in clientRooms) {
+            console.log("Room message " + clientRooms[client.id]);
+            client.to(clientRooms[client.id]).emit('receive-message', msg);
         }
         else {
-            console.log("public")
-            socket.broadcast.emit('chat message', msg)
+            console.log("Send message to all")
+            client.broadcast.emit('receive-message', msg)
         }
-    });
-    socket.on('disconnect', () => {
-        console.log('user disconnected')
-    });
+    }
 
-    socket.on('start-room', () => {
-        room = new Room(socket.id, [socket.id])
-        rooms.addRoom(room);
-    });
+    function startRoom() {
+        roomCode = generateRoomCode();
 
-    socket.on('join-room', () => {
-        openRoom = rooms.findOpenRoom();
-        if (openRoom) {
-            openRoom.joinRoom(socket.id);
-            socket.join(openRoom.roomId);
-            console.log("joining room: " + openRoom.roomId)
+        if (moveClientToRoom(client, roomCode)) {
+            client.emit("receive-message", "Created room with code: " + roomCode);
         }
-    })
+    }
+
+    function joinRoom(roomCode) {
+        console.log(client.id + " joining room " + roomCode);
+        const room = io.of('/').adapter.rooms.get(roomCode);
+
+        let roomClients = room ? room : null;
+        let numClients = roomClients ? roomClients.size : 0;
+
+        if (numClients == 0) {
+            // Room doesn't exist
+            client.emit("receive-message", "Couldn't find room with code " + roomCode);
+        }
+        else if (numClients >= MAX_PLAYERS) {
+            // Room is already full
+            client.emit("receive-message", "That room is already full!");
+        }
+        else {
+            // Room exists and isn't full
+            if (moveClientToRoom(client, roomCode)) {
+                client.emit("receive-message" , "Joined room with code: " + roomCode);
+            }
+        }
+    }
 })
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
 })
 
+function moveClientToRoom(client, roomCode) {
+    // If they are in a room
+    if (client.id in clientRooms) {
+        client.emit("receive-message", "You are already in a room!");
+        return false
+    }
+    else {
+        clientRooms[client.id] = roomCode;
+        client.join(roomCode);
+        return true
+    }
+}
 
+function generateRoomCode() {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let roomCode = ""
+    do {
+        // Generate a 4 character room code
+        roomCode = "";
+        for (let i = 0; i < 4; i++) {
+            roomCode += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+    } while (io.of('/').adapter.rooms.has(roomCode)) // If room exists, try again
+
+    return roomCode
+}
