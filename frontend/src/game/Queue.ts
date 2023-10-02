7dffdfa0d9fd6ae3e550bbcf54e0c8253a4e248d
Peter Jin <peterj9h@gmail.com>
2023-10-01 22:15:40 -0400

    minor style changes

--- frontend/src/game/Queue.ts
+++ frontend/src/game/Queue.ts
@@ -3,21 +3,25 @@ import { Card } from "./Card";
 
 export class Queue extends Container {
     
-    /* Color of this card queue */
+    /** Color of this card queue */
     public color!: number;
 
-    /* Contains queue of Card objects */
+    /** Contains queue of Card objects */
     public cardQueue: Card[] = [];
 
     constructor() {
         super();
     }
 
+    /** 
+     * Create a new card queue based on a list of cards
+     * Performs a deep copy of the given card list 
+     */
     public fill(cardList: Card[]) {
         this.cardQueue = structuredClone(cardList);
     }
 
-    /* Receives played card and removes and returns new one from front of queue */
+    /** Receives played card and removes and returns new one from front of queue */
     public addCard(card: Card) {
         this.cardQueue.push(card);
         return this.cardQueue.shift();
