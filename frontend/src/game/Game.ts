3dce4c3acdb1c34b7e7a85e7ab4c449bba16806e
CyrilSharma <sharma.cyril@gmail.com>
2023-10-13 15:28:13 -0400

    added color and turn display

--- frontend/src/game/Game.ts
+++ frontend/src/game/Game.ts
@@ -1,15 +1,17 @@
 import { Container, Sprite } from 'pixi.js';
 import { Board } from './Board';
-import { GameConfig } from './Utils';
+import { ColorEnum, GameConfig } from './Utils';
 import { app } from '../main';
 import { CardQueue } from './CardQueue';
 import { GameUpdate } from './GameUpdate';
+import { GameState } from './GameState';
 // This seems a little redundant right now,
 // But it will house the cards as well,
 // And provide some callbacks maybe.
 export class Game extends Container {
     public board: Board;
     public cards: CardQueue;
+    public gameState: GameState;
     public config!: GameConfig;
     public playerColor!: number;
     public updateList: GameUpdate[] = [];
@@ -18,8 +20,10 @@ export class Game extends Container {
         super();
         this.board = new Board(this);
         this.cards = new CardQueue(this);
+        this.gameState = new GameState(this);
         this.addChild(this.board);
         this.addChild(this.cards);
+        this.addChild(this.gameState);
     }
 
     public setup(config: GameConfig) {
@@ -27,16 +31,20 @@ export class Game extends Container {
         this.board.setup(config);
         this.cards.setup();
         this.cards.y = this.board.getHeight();
+        this.gameState.setup();
+        this.gameState.y = this.cards.y + this.cards.height;
     }
 
     public setPlayerColor(color: number) {
         this.playerColor = color;
+        this.gameState.updateColor(color);
     }
 
     public updateTurn() {
         this.turn += 1;
         if (this.turn > 6) this.turn -= 6;
         console.log(`turn: ${this.turn}`);
+        this.gameState.updateTurn(this.turn);
     }
 
     public ourTurn() {
---
+++ frontend/src/game/GameState.ts
@@ -0,0 +1,44 @@
+import { Container, Graphics, Sprite, Text } from 'pixi.js';
+import { Piece } from './Piece';
+import { Game } from './Game';
+import { GameConfig } from './Utils';
+
+export class GameState extends Container {
+    public background: Graphics;
+    public game: Game;
+    public color: Text;
+    public turn: Text;
+
+    // We pass the game to allow for callbacks...
+    constructor(game: Game) {
+        super();
+        this.game = game;
+        this.background = new Graphics();
+        this.turn = new Text("OOWIEHRIUWEHRIF");
+        this.color = new Text("IWUEFHUIWEH");
+        this.addChild(this.background);
+        this.addChild(this.turn);
+        this.addChild(this.color);
+    }
+
+    public setup() {
+        this.background.beginFill(0x964B00);
+        this.background.drawRect(0, 0, this.game.board.getWidth(), 50);
+        this.updateTurn(this.game.turn);
+        this.turn.x = this.game.board.getWidth() / 8; 
+        this.color.x = 5 * this.game.board.getWidth() / 8;
+    }
+
+    public updateTurn(turn: number) {
+        this.turn.text = "Turn: " + turn;
+    }
+    
+    public updateColor(color_id: number) {
+        let color = "";
+        if (color_id == 1) color = "Red";
+        else if (color_id == 2) color = "Yellow";
+        else if (color_id == 3) color = "Blue";
+        else if (color_id == 4) color = "Purple";
+        this.color.text = "Color: " + color;
+    }
+}
