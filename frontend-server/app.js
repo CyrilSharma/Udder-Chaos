92107a7734c11869c24747dae2bc93b853590d5e
CyrilSharma <sharma.cyril@gmail.com>
2023-10-22 13:06:01 -0400

    add images

--- frontend-server/app.js
+++ frontend-server/app.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 80
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 })
