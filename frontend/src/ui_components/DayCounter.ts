ec8b0ae11e91ebab67f44ae3a7b95cf5a82876a7
epweiss19 <100245684+epweiss19@users.noreply.github.com>
2023-11-11 16:43:32 -0500

    Made Day Counter work with any number of days from 4 to 20 (3 - 19 segments) <5.5 hrs>
    Day counter now works with any reasonable number of days that allow for the triangle theme to work. (ie cannot go below 3 triangles).
    Please do not ask for generalized version. I tried for 3 hours to do so.

--- frontend/src/ui_components/DayCounter.ts
+++ frontend/src/ui_components/DayCounter.ts
@@ -6,20 +6,25 @@ export class DayCounter extends FancyButton {
 
     private days: Array<RoundedTriangle>;
     private label: Text;
+    public totDays: number;
     public day: number;
 
-    constructor() {
+
+    constructor(days: number) {
         super();
 
         this.day = 0;
+        this.totDays = days;
         //console.log(this.day);
 
         this.days = new Array<RoundedTriangle>;
-        for (let i = 0; i < 6; i++) {
-            this.days[i] = new RoundedTriangle();
-            this.days[i].rotateTriangleDeg(30 + i * 60);
-            this.days[i].x = Math.cos((60 - 60 * i) * Math.PI / 180) * 80;
-            this.days[i].y = Math.sin((60 - 60 * i) * Math.PI / 180) * -80;
+        for (let i = 0; i < days - 1; i++) {
+            this.days[i] = new RoundedTriangle(days - 1);
+            this.days[i].rotateTriangleDeg((180 / (days - 1)) + i * 360 / (days - 1));
+            this.days[i].x = Math.cos(((360 / (days - 1)) - (360 / (days - 1)) * (i)) * Math.PI / 180) * 100;
+            this.days[i].y = Math.sin((-(360 / (days - 1)) + (360 / (days - 1)) * (i)) * Math.PI / 180) * 100;
+            //this.days[i].x = 0//Math.
+            //this.days[i].y = 0//- this.days[i].triangleOff.width * 0.5 * Math.cos(i * 2 * Math.PI / (days - 1)) - this.days[i].triangleOff.height * 0.5 * Math.cos(i * 2 * Math.PI / (days - 1));
             this.addChild(this.days[i]);
         }
         this.label = new Text("Day Tracker", new TextStyle({
@@ -29,12 +34,12 @@ export class DayCounter extends FancyButton {
             align: "left",
         }));
         this.label.x = -100;
-        this.label.y = -190;
+        this.label.y = -250;
         this.addChild(this.label);
     }
     
     public cycleDay(counter: DayCounter) {
-        if (counter.day == 6) {
+        if (counter.day == counter.totDays - 1) {
             counter.days.forEach(ele => {
                 ele.turnOff();
             });
