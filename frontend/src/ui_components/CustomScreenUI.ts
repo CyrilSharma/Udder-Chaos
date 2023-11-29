43d5d4df8a24eb6baf0d0cec1d16a53b00801423
epweiss19 <100245684+epweiss19@users.noreply.github.com>
2023-11-28 23:31:42 -0500

    Added Game Customization Screen Settings<3.5 hrs>
    Added options to set deck size, ai difficulty, days per round, cows per sacrifice, cows for a win, and cow respawn time to the customization screen.
    ALSO added the slider component

--- frontend/src/ui_components/CustomScreenUI.ts
+++ frontend/src/ui_components/CustomScreenUI.ts
@@ -3,25 +3,29 @@ import { MenuContainer } from "./MenuContainer";
 import { BackButton } from "./BackButton";
 import { SizedButton } from "./SizedButton";
 import { SeedBox } from '../ui_components/SeedBox';
-
+import { SliderUI } from './SliderUI';
+import { Background } from "./Background";
 
 export class CustomScreenUI extends Container {
 
-    private vignette: Graphics;
+    private vignette: Background;
     private menuContainer: MenuContainer;
     private backButton: BackButton;
     private customLabel: SizedButton;
     private seedBox: SeedBox;
-
+    private deckSize: SliderUI;
+    private difficulty: SliderUI;
+    private daysPerRound: SliderUI;
+    private cowSacrificeAmt: SliderUI;
+    private cowsWin: SliderUI;
+    private cowsRespawn: SliderUI;
 
     constructor() {
         super();
 
         this.visible = false;
 
-        this.vignette = new Graphics()
-            .beginFill(0x000000)
-            .drawRect(0,0,10,10);
+        this.vignette = new Background();
         this.addChild(this.vignette);
 
         this.menuContainer = new MenuContainer();
@@ -39,21 +43,91 @@ export class CustomScreenUI extends Container {
         this.seedBox = new SeedBox(this.menuContainer, 0.78, 0.12, 0.3, 0.15, "Seed", 6);
         this.menuContainer.addChild(this.seedBox);
 
+        this.deckSize = new SliderUI(0.25, 0.4, 0.45, 0.2, this.menuContainer.width, this.menuContainer.height, "Deck Size", 10, 20, 30, this.menuContainer.getBox());
+        this.menuContainer.addChild(this.deckSize);
+
+        this.difficulty = new SliderUI(0.25, 0.6, 0.45, 0.2, this.menuContainer.width, this.menuContainer.height, "AI Difficulty", 1, 5, 30, this.menuContainer.getBox());
+        this.menuContainer.addChild(this.difficulty);
+
+        this.daysPerRound = new SliderUI(0.25, 0.8, 0.45, 0.2, this.menuContainer.width, this.menuContainer.height, "Days Per Round", 4, 18, 30, this.menuContainer.getBox());
+        this.menuContainer.addChild(this.daysPerRound);
+
+        this.cowSacrificeAmt = new SliderUI(0.75, 0.4, 0.45, 0.2, this.menuContainer.width, this.menuContainer.height, "Cows per Sacrifice", 1, 20, 30, this.menuContainer.getBox());
+        this.menuContainer.addChild(this.cowSacrificeAmt);
+
+        this.cowsWin = new SliderUI(0.75, 0.6, 0.45, 0.2, this.menuContainer.width, this.menuContainer.height, "Cows Needed For Win", 1, 50, 30, this.menuContainer.getBox());
+        this.menuContainer.addChild(this.cowsWin);
+
+        this.cowsRespawn = new SliderUI(0.75, 0.8, 0.45, 0.2, this.menuContainer.width, this.menuContainer.height, "Cow Respawn Time", 1, 30, 30, this.menuContainer.getBox());
+        this.menuContainer.addChild(this.cowsRespawn);
+
     }
 
     public getSeed() {
         return this.seedBox.seed.value;
     }
 
+    public getDeckSize() : number {
+        return this.deckSize.getValue();
+    }
+
+    public setDeckSize(val: number) {
+        this.deckSize.setValue(val);
+    }
+
+    public getDifficulty() : number {
+        return this.difficulty.getValue();
+    }
+
+    public setDifficulty(val: number) {
+        this.difficulty.setValue(val);
+    }
+
+    public getDaysPerRound() : number {
+        return this.daysPerRound.getValue();
+    }
+
+    public setDaysPerRound(val: number) {
+        this.daysPerRound.setValue(val);
+    }
+
+    public getCowSacrificeAmt() : number {
+        return this.cowSacrificeAmt.getValue();
+    }
+
+    public setCowSacrificeAmt(val: number) {
+        this.cowSacrificeAmt.setValue(val);
+    }
+
+    public getCowsForWin() : number {
+        return this.cowsWin.getValue();
+    }
+
+    public setCowsForWin(val: number) {
+        this.cowsWin.setValue(val);
+    }
+
+    public getCowRespawnRate() : number {
+        return this.cowsRespawn.getValue();
+    }
+    
+    public setCowRespawnRate(val: number) {
+        this.cowsRespawn.setValue(val);
+    }
+
     public resize(width: number, height: number) {
-        this.vignette.width = width;
-        this.vignette.height = height;
+        this.vignette.resize(width, height);
         this.menuContainer.resize(width, height);
         this.backButton.resize(this.menuContainer.getBox());
         this.customLabel.resize(this.menuContainer.getBox());
         this.seedBox.resize(this.menuContainer.getBox());
 
-
+        this.deckSize.resize(this.menuContainer.getBox());
+        this.difficulty.resize(this.menuContainer.getBox());
+        this.daysPerRound.resize(this.menuContainer.getBox());
+        this.cowSacrificeAmt.resize(this.menuContainer.getBox());
+        this.cowsWin.resize(this.menuContainer.getBox());
+        this.cowsRespawn.resize(this.menuContainer.getBox());
     }
 
 
---
+++ frontend/src/ui_components/SliderUI.ts
@@ -0,0 +1,144 @@
+import { Container, FederatedPointerEvent, Text, TextStyle } from "pixi.js";
+import { SizedButton } from "./SizedButton";
+
+export class SliderUI extends Container {
+
+    private slide: SizedButton;
+    private slider: SizedButton;
+
+    private min: number;
+    private max: number;
+    private value: number;
+
+    private label: Text;
+    private minLabel: Text;
+    private maxLabel: Text;
+    private valueLabel: Text;
+
+    private pBounds: Array<number>;
+
+    constructor(x: number, y: number, width: number, height: number, parentW: number, parentH: number, label: string, min: number, max: number, fontSize: number, bounds: Array<number>) {
+        super();
+
+        this.slide = new SizedButton(x, y, width, height / 3, "", parentW, parentH, 10, 0x50a0d0);
+        this.addChild(this.slide);
+
+        this.slider = new SizedButton(x - width / 2 + height / 10, y, height / 5, height / 3, "", parentW, parentH, 10, 0x50d0a0);
+        this.addChild(this.slider);
+
+        this.min = min;
+        this.max = max;
+        this.value = min;
+
+        this.label = new Text(label, new TextStyle({
+            fontFamily: "Concert One",
+            fontSize: fontSize,
+            fill: "#ffffff",
+            align: "center",
+        }));
+        this.addChild(this.label);
+
+        this.minLabel = new Text(min, new TextStyle({
+            fontFamily: "Concert One",
+            fontSize: 2 * fontSize / 3,
+            fill: "#ffffff",
+            align: "center",
+        }));
+        this.addChild(this.minLabel);
+
+        this.maxLabel = new Text(max, new TextStyle({
+            fontFamily: "Concert One",
+            fontSize: 2 * fontSize / 3,
+            fill: "#ffffff",
+            align: "center",
+        }));
+        this.addChild(this.maxLabel);
+
+        this.valueLabel = new Text(this.value, new TextStyle({
+            fontFamily: "Concert One",
+            fontSize: fontSize / 1.1,
+            fill: "#8866cc",
+            align: "center",
+        }));
+        this.addChild(this.valueLabel);
+
+        this.slider.onDown.connect(() => {
+            const sliderUI = this;
+
+            document.addEventListener("pointermove", moveSlider, false);
+            function moveSlider(e: PointerEvent) {
+                sliderUI.setSlide(e.x);
+            }
+
+            this.slider.onUp.connect(() => {
+                document.removeEventListener("pointermove", moveSlider, false);
+            });
+            
+        });
+
+        this.pBounds = bounds;
+    }
+
+    public getValue() {
+        return this.value;
+    }
+    
+    public setValue(value: number) {
+        if (value < this.min) {
+            this.value = this.min;
+        } else if (value > this.max) {
+            this.value = this.max;
+        } else {
+            this.value = value;
+        }
+        this.valueLabel.text = this.value;
+        let increment = (this.slide.width - this.slider.width) / (this.max - this.min);
+        let diff = this.value - this.min;
+        this.slider.setX(((this.slide.x - this.slide.width / 2) + (diff * increment) - this.slider.width) / (this.pBounds[3] - this.pBounds[2]), this.pBounds);
+    }
+
+    private setSlide(x: number) {
+        let increment = (this.slide.width - this.slider.width) / (this.max - this.min);
+        let dist = 100000;
+        let index = 0;
+        for (let i = 0; i < this.max - this.min + 1; i++) {
+            let tmp = Math.abs(x - ((this.slide.x) - (this.slide.width)/2 + this.slider.width/2 + i * increment));
+            if (tmp < dist) {
+                dist = tmp;
+                index = i;
+            } else {
+                break;
+            }
+        }
+        let tmp = (index * increment) + (this.slide.x - this.slide.width / 2 + this.slider.width / 2);
+        let tmpX = tmp - this.pBounds[2];
+        tmpX /= (this.pBounds[3] - this.pBounds[2]);
+        this.slider.setX(tmpX, this.pBounds);
+        this.value = this.min + index;
+        this.updateValue();
+    }
+
+    private updateValue() {
+        this.valueLabel.text = this.value;
+    }
+
+    public resize(bounds: Array<number>) {
+        // Top, bottom, left, right
+        this.pBounds = bounds;
+
+        this.slide.resize(bounds);
+        this.slider.resize(bounds);
+
+        this.label.x = this.slide.x - this.label.width / 2;
+        this.label.y = this.slide.y - this.label.height / 2 - this.slide.height;
+
+        this.minLabel.x = this.slide.x - this.slide.width / 2 + this.minLabel.width / 2;
+        this.minLabel.y = this.slide.y - this.label.height / 2 + this.slide.height;
+
+        this.maxLabel.x = this.slide.x + this.slide.width / 2 - 3 * this.maxLabel.width / 2;
+        this.maxLabel.y = this.slide.y - this.label.height / 2 + this.slide.height;
+
+        this.valueLabel.x = this.slide.x - this.valueLabel.width / 2;
+        this.valueLabel.y = this.slide.y - this.valueLabel.height / 2 + this.slide.height;
+    }
+}
